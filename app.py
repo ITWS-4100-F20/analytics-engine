4663f8bf87de99a4d531ab95c2e18eae3746aff3
Noah Deahl <Noah.Deahl@ibm.com>
2020-12-05 14:16:52 -0500

    Initialize flask server stub

--- app.py
+++ app.py
@@ -5,6 +5,11 @@ from simulation.support.database import client
 from simulation.environment.scenario import Scenario
 from simulation.support.util import *
 
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
 def startSim():
     startTime = time.strptime("12/01/2020 01:00:00", "%d/%m/%Y %H:%M:%S")
     endTime = time.strptime("12/01/2020 22:00:00", "%d/%m/%Y %H:%M:%S")
@@ -13,5 +18,10 @@ def startSim():
         , scenario["manifestId"], scenario["cabins"], scenario["flightid"], scenario["flightnum"])
     simulation.runSimulation(testScenario)
 
+@app.route('/simulation')
+def simulation():
+    if request.method == 'POST':
+        print('POST')
+
 if __name__ == '__main__':
-    startSim()
+    app.run(port=3031)
