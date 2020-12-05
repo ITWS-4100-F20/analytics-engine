c9bb716bfd1ed89922f8d559dbeb7d31f702c3a5
meyern4 <meyern4@rpi.edu>
2020-12-04 21:19:27 -0500

    added dynamic loading of data from the database. Currently the data is loaded from nick-test type areas but can be easily changed

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 #this will be used to host the FLASK api server
 import time
 from simulation import simulation
+from simulation.support.database import client
 from simulation.environment.scenario import Scenario
+from simulation.support.util import *
 
 def startSim():
     startTime = time.strptime("12/01/2020 01:00:00", "%d/%m/%Y %H:%M:%S")
     endTime = time.strptime("12/01/2020 22:00:00", "%d/%m/%Y %H:%M:%S")
-    testScenario = Scenario(time.mktime(startTime), time.mktime(endTime), "EWR", "ALB", 1)
+    scenario = getScenario('nick_test')
+    testScenario = Scenario(time.mktime(startTime), time.mktime(endTime), scenario["start"], scenario["dest"]
+        , scenario["manifestId"], scenario["cabins"], scenario["flightid"], scenario["flightnum"])
     simulation.runSimulation(testScenario)
 
 if __name__ == '__main__':
--- simulation/environment/scenario.py
+++ simulation/environment/scenario.py
@@ -2,11 +2,14 @@ class Scenario(object):
     def __init__(self, 
         oversaleStartTime: int, flightBoardingTime: int, 
         departureAirport: str, arrivalAirport: str,
-        aircraftManifestID: int
+        aircraftManifestID: int, cabins: dict, flightid: int, flightnum: int
     ):
         self.oversaleStartTime = oversaleStartTime
         self.flightBoardingTime = flightBoardingTime
         self.departureAirport = departureAirport
         self.arrivalAirport = arrivalAirport
         self.aircraftManifestID = aircraftManifestID
+        self.cabins = cabins
+        self.flightid = flightid
+        self.flightnum = flightnum
 
