abd4f99980b7edb76298ccf6abb4c8a0f6867184
Noah Deahl <Noah.Deahl@ibm.com>
2020-12-11 18:22:19 -0500

    Add model def endpoint

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import threading
 from simulation.simulation import Simulation
+from simulation.support.modelHelper import createModels
 from flask import Flask, request
 
 app = Flask(__name__)
@@ -14,6 +15,12 @@ def simulation():
     scen = Simulation(data["scenarioId"], data["parameters"])
     thread = threading.Thread(target=__startSim, kwargs={'scen' : scen}).start()
     return {"sim_id" :scen.uuid }
+
+@app.route('/modelDefinition', methods=["POST"])
+def simulation():
+    data = dict(request.json)
+    createModels(data["def_name"],data["data_schema_name"],data["passenger_target"],data["comp_target"],data["ignore_pass"],data["ignore_comp"])
+    return "ok", 200
 
 if __name__ == '__main__':
     app.run(port=3031, host='0.0.0.0')
